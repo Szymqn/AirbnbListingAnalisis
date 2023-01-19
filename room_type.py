fd985aeb71260b58f1b15a74a31d3432ce1a262a
szymqn <koodziejskisz@outlook.com>
2023-01-19 15:13:54 +0100

    add percent to graph

--- room_type.py
+++ room_type.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 
 def load():
@@ -17,20 +18,22 @@ def load():
 
 def analize(source):
     data = source.value_counts()
-
-    data_dict = data.to_dict()
-
-    fig, ax = plt.subplots()
-
-    t_keys = data_dict.keys()
     keys = []
 
+    data_dict = data.to_dict()
+    t_keys = data_dict.keys()
+
+    # Convert and add keys to list of keys
     for key in t_keys:
         keys.append(*key)
 
     values = data_dict.values()
 
+    fig, ax = plt.subplots()
+
     ax.bar(keys, values)
     ax.set_title('Room types')
+
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=max(values)))
 
     plt.show()
