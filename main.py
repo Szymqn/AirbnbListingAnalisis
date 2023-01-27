b326ec1bfecd4de6bd43b747d3fe927ebc39c5a6
szymqn <koodziejskisz@outlook.com>
2023-01-28 00:10:37 +0100

    create template for reviews

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from room_type import load_room_type
 from host_rate import load_host_rate
+from review_score import load_review_score
 
 
 if __name__ == '__main__':
     load_room_type()
     load_host_rate()
+    load_review_score()
---
+++ review_score.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+def load_review_score():
+    HERE = Path(__file__).parent
+    DATA_FOLDER = HERE / "data"
